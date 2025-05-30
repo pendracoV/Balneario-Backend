cf5bb399ea649438d032f19b15ff853fefdb61bf
StayDead <thomasbarrios7@hotmail.com>
2025-05-30 01:26:09 -0500

    turno

--- src/controllers/turnoController.js
+++ src/controllers/turnoController.js
@@ -1,23 +1,19 @@
 // src/controllers/turnoController.js
-const Turno = require('../models/Turno');
+const Turno = require('../models/Turno')
 
 exports.create = async (req, res, next) => {
   try {
-    const { personalId, fecha } = req.body;
+    const { personalId, fecha } = req.body
     if (!personalId || !fecha) {
-      return res
-        .status(400)
-        .json({ message: 'Debe enviar personalId y fecha' });
+      return res.status(400).json({ message: 'Debe enviar personalId y fecha' })
     }
 
-    // Los tres bloques que siempre queremos crear
     const bloques = [
-      { horario_inicio: '09:00', horario_fin: '12:00' },
-      { horario_inicio: '14:00', horario_fin: '18:00' },
-      { horario_inicio: '18:00', horario_fin: '23:00' },
-    ];
+      { horario_inicio: '09:00:00', horario_fin: '12:00:00' },
+      { horario_inicio: '14:00:00', horario_fin: '18:00:00' },
+      { horario_inicio: '18:00:00', horario_fin: '23:00:00' }
+    ]
 
-    // Crear los 3 turnos en paralelo
     const turnos = await Promise.all(
       bloques.map(b =>
         Turno.create({
@@ -27,11 +23,15 @@ exports.create = async (req, res, next) => {
           horario_fin: b.horario_fin
         })
       )
-    );
+    )
 
-    res.status(201).json(turnos);
-
+    res.status(201).json(turnos)
   } catch (err) {
-    next(err);
+    if (err.name === 'SequelizeValidationError') {
+      return res.status(400).json({
+        message: err.errors.map(e => e.message).join('; ')
+      })
+    }
+    next(err)
   }
-};
+}
